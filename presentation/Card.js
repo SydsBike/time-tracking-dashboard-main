33820eba9005ab7a7039e9a658bdc2bd358e2f16
Your Name <youremail@example.com>
2025-01-12 13:09:34 -0600

    update ellipsis src

--- presentation/Card.js
+++ presentation/Card.js
@@ -1,14 +1,14 @@
 const { useContext } = React;
 
 function Card({ title, data }) {
-  const { timeframe } = useContext(AppData);
+  const { timeframe, images } = useContext(AppData);
   return (
     <section className={`card ${title.toLowerCase().replace(" ", "-")}`}>
       <Image title={title} />
       <div className="card-content">
         <h2 className="fs-h2 fc-white fw-bold title">{title}</h2>
         <span className="dots">
-          <img src="../images/icon-ellipsis.svg" />
+          <img src={images.ellipsis} />
         </span>
         <p className="fs-current fc-white fw-light current">{data.timeframes[timeframe].current}hrs</p>
         {window.innerWidth > 1080 ? <br /> : ""}
